a6f48beffd478d9909e370304f8be3fb0eb8c308
Julia Miachina <juliarose96061@gmail.com>
2026-04-19 00:28:04 +0300

    First login test added

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+
+    }
+    @AfterMethod
+    public void close(){
+        driver.quit();
+
+    }
+
+}
